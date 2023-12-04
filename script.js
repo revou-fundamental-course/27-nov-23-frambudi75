5b47d42d0894b9c464a0678535a0844a160c1a04
Habib Frambudi <59114869+frambudi75@users.noreply.github.com>
2023-12-04 18:31:00 +0700

    new update code

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-
+// script.js
 function hitung() {
-    // Ambil nilai dari input
     const sideA = parseFloat(document.getElementById('sideA').value);
     const sideB = parseFloat(document.getElementById('sideB').value);
     const sideC = parseFloat(document.getElementById('sideC').value);
   
-    // Validasi input (semua sisi harus lebih dari 0)
     if (sideA > 0 && sideB > 0 && sideC > 0) {
-      // Hitung luas 
-      const s = (sideA + sideB + sideC) / 2;
-      const luas = Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
-  
-      // Hitung keliling
       const keliling = sideA + sideB + sideC;
   
-      //  hasil
       const resultDiv = document.getElementById('result');
-      resultDiv.innerHTML = `
-        <h2>Hasil Perhitungan</h2>
-        <p>Luas Segitiga: ${luas.toFixed(2)}</p>
-        <p>Keliling Segitiga: ${keliling.toFixed(2)}</p>
-      `;
+      resultDiv.innerHTML = `<p>Keliling Segitiga: ${keliling.toFixed(2)}</p>`;
     } else {
       alert('Sisi-sisi harus memiliki nilai yang valid (lebih dari 0).');
     }
